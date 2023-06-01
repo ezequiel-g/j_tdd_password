334661c1b516955893f19ad554cb824eb8ea8cab
ezequiel-g <ezequielghione@gmail.com>
2023-05-31 23:24:25 -0300

    adding src

---
+++ src/main/java/poc/tdd/Main.java
@@ -0,0 +1,9 @@
+package poc.tdd;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/poc/tdd/PasswordValidatorTest.java
@@ -0,0 +1,14 @@
+package poc.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PasswordValidatorTest {
+
+    @Test
+    void test1() {
+        assertThat(1).isEqualTo(1);
+    }
+
+}
